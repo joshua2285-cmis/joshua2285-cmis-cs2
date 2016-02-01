565bbbe5e74b6481b81bb7e79068d03ea64d21a2
cmis <cmis@cmis-hs15.(none)>
2016-02-01 13:37:22 +0700

    Almost finish with the assignment

--- assignment1.py
+++ assignment1.py
@@ -1,11 +1,50 @@
-myname="Joshua"
+# My name
+myname="Joshua."
+
+# My age
 myage=17.25
-myhight=1.69
-square="x"
+
+# My hight
+myhight= 1.69
+
+# One side of square
+square= 4
+
+# Length of a rectangle
 length=10
+
+# Hight of a rectangle
 hight=15
+
+# My age in Month
 ageinmonth= myage * 12
+
+# My Life expectancy
 lifeexpectancy=83
+
+# Years to live for me
 yeartolive= lifeexpectancy-myage
+
+# My hight in feet
 hightinfeet = myhight * 3
-hightofotherperson = 170
+
+# Hight of another person
+hightofotherperson = 1.70
+
+# Difference between me and the acerage hight
+differencebetweenmeandaverage = hightofotherperson - myhight
+
+# Area of the square
+areaofsquare = square * square
+
+# Volume of half a cube
+cube = areaofsquare * square / 2
+
+# Area of rectangle
+areaofrectangle = length * hight * 1/9
+
+# Introduction of my self
+print "Hello. my name is " + myname + " I am " + str(myhight) + "m tall. I am " + str(myage) + " years old. I have approximately " + str(yeartolive) + " years to live. My hight is adverage compare to other Hong Kong people, the difference between the adverage hight and my hight is " + str(differencebetweenmeandaverage) + " m."
+
+# Smiley faces
+print ";)" * 10000
