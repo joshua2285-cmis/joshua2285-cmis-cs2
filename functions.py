df31c43b53a719032839b6d9c32f7a71a8dc8fc4
cmis <cmis@cmis-hs15.(none)>
2016-02-09 08:39:54 +0700

    finished the first three step

---
+++ functions.py
@@ -0,0 +1,21 @@
+# Adding funcion
+def add(a,b):
+	return a+b
+
+add(7,4)
+add(add(add(7,8),add(6,1)),add(4,7))
+
+# subtraction function
+def sub(a,b):
+	return a-b
+
+sub(7,6)
+sub(sub(sub(79,4),16),sub(9,3))
+
+# Multiplcation function
+def mul(a,b):
+	return a*b
+
+mul(7,3)
+mul(mul(mul(mul(74,1),mul(12,756)),0),100000)
+
