76d1dd83001f009ceedeb9007f0665e2c389eb8a
cmis <cmis@cmis-hs15.(none)>
2016-02-10 10:57:51 +0700

    finish until volume of sphere, got stuck with 4/3

--- functions.py
+++ functions.py
@@ -19,3 +19,29 @@ def mul(a,b):
 mul(7,3)
 mul(mul(mul(mul(74,1),mul(12,756)),0),100000)
 
+# Division
+def div(a,b):
+	return float(a)/b
+
+div(22,7)
+div(143, 43)
+
+# Seconds to Hours
+def hours_from_seconds(x):
+	return x/3600
+
+hours_from_seconds(86400)
+
+# Area of a circle
+import math
+def circle_area(a):
+	return math.pi*a**2
+
+circle_area(5)
+
+# Volume of sphere
+import math
+def sphere_volume(s):
+	return (s**3)*math.pi*(4/3)
+
+print sphere_volume(5)
