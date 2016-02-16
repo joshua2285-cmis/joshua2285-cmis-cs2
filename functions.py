e9d460e8c9564c187b218a35c8be78b045b64292
cmis <cmis@cmis-hs15.(none)>
2016-02-16 13:36:30 +0700

    Almost done only need to change the variable and print output

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 #import
 from math import sqrt
 from math import pi
+import easygui
 # Adding funcion
 def add(a,b):
 	return a+b
@@ -60,9 +61,42 @@ def area(l1,l2,l3):
 
 print area(1, 2, 2.5)
 
-# Hello
-def right_align(z):
-	return len
+# Hello right align
+def right_align(r):
+	return " " * (80 - int(len(r))) + str(r)
 
 print right_align("Hello")
+
+# Hello center
+def center(z):
+	return " " * (40 - int(len(z)/2)) + str(z)
+
+print center("Hello")
+
+# Message Box
+
+def msg_box(a):
+	return easygui.msgbox(a)
+
+print msg_box("Hello")
+print msg_box("I eat cats!")
+
+# call functions twice
+adding01 = add(7,4) 
+adding02 = add(add(add(7,8),add(6,1)),add(4,7))
+subtract01 = sub(7,6)
+subtract02 = sub(sub(sub(79,4),16),sub(9,3))
+multiply01 = mul(7,3)
+multiply02 = mul(mul(mul(mul(74,1),mul(12,756)),0),100000)
+division01 = div(22,7)
+division02 = div(143, 43) 
+hours_from_second = hours_from_seconds(86400)
+circle_area = circle_area(5)
+avg_volume = avg_volume(10,20)
+area = area(1, 2, 2.5)
+right_align = right_align("Hello")
+center = center("Hello")
+msg_box01 = msg_box("Hello")
+msg_box02 = msg_box("I eat cats!")
+
 
