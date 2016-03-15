8ea0c9daba7cc69a99974abe627d67083f28f58d
cmis <cmis@cmis-hs15.(none)>
2016-03-15 13:35:40 +0700

    almost done

--- oneguess.py
+++ oneguess.py
@@ -11,13 +11,31 @@ def main():
 	maxnumber = raw_input("What is the maximum number? ")
 
 	print """
-I am thinking of a number from  {} to {}
+I am thinking of a number from  {} to {}.
 """ .format(minnumber, maxnumber)
 
 	guessnumber = raw_input("What do you think it is?: ")
-
-
+	undernumber = random.randint(int(minnumber), int(maxnumber)) - int(guessnumber)
+	overnumber = int(guessnumber) - random.randint(int(minnumber), int(maxnumber))
 	
+	if thenumber > guessnumber:
+		print """
+The target was {}.
+Your guess was {}.
+That's under by {}.
+""" .format(thenumber, guessnumber, undernumber )
+	elif thenumber < guessnumber:
+		print """
+The target was {}.
+Your guess was {}.
+That's over by {}.
+""" .format(thenumber, guessnumber, overnumber)
+	elif thenumber == guessnumber:
+			print """
+The target was {}.
+Your guess was {}.
+That's correct! You must be psychic!
+""" .format(thenumber, guessnumber)
 
-#Main()
+main()
 
