75cf8e6d1b3d6b5c6c150900ac467e84758be895
cmis <cmis@cmis-hs04.(none)>
2016-03-17 08:44:20 +0700

    something's missing

--- oneguess.py
+++ oneguess.py
@@ -1,41 +1,39 @@
 #Import
 import random
 
-# The random number
-def thenumber(minnumber, maxnumber):
-	return random(minnumber, maxnumber)
 
 #Main
 def main():
-	minnumber = raw_input("What is the minimum number? ")
-	maxnumber = raw_input("What is the maximum number? ")
-
+	minnumber = int(raw_input("What is the minimum number? "))
+	maxnumber = int(raw_input("What is the maximum number? "))
+	thenumber = int(random.randint(minnumber, maxnumber))
 	print """
 I am thinking of a number from  {} to {}.
 """ .format(minnumber, maxnumber)
 
-	guessnumber = raw_input("What do you think it is?: ")
-	undernumber = random.randint(int(minnumber), int(maxnumber)) - int(guessnumber)
-	overnumber = int(guessnumber) - random.randint(int(minnumber), int(maxnumber))
+	guessnumber = int(raw_input("What do you think it is?: "))
+	wrongby = abs(thenumber - guessnumber)
 	
-	if thenumber > guessnumber:
-		print """
-The target was {}.
-Your guess was {}.
-That's under by {}.
-""" .format(thenumber, guessnumber, undernumber )
-	elif thenumber < guessnumber:
-		print """
-The target was {}.
-Your guess was {}.
-That's over by {}.
-""" .format(thenumber, guessnumber, overnumber)
-	elif thenumber == guessnumber:
+	def findoverorunder():
+		if thenumber < guessnumber:
+			overorunder == "over"
+		elif thenumber > guessnumber:
+			overorunder == "under"
+		return overorunder
+
+	if thenumber == guessnumber:
 			print """
 The target was {}.
 Your guess was {}.
 That's correct! You must be psychic!
 """ .format(thenumber, guessnumber)
+
+	elif thenumber != guessnumber :
+		print """
+The target was {}.
+Your guess was {}.
+That's {} by {}.
+""" .format(thenumber,guessnumber, overorunder,  wrongby)
 
 main()
 
