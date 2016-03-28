2b738e136f488aaece858ae08f88a2453b766baa
cmis <cmis@cmis-hs04.(none)>
2016-03-28 10:53:43 +0700

    Just begin on conditionals

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+# you are locked in a room and there are many different doors with passcode and you are trying to break free. 
+
+#import
+import random
+
+
+def coding():
+	if incertdcode == correctpass:
+		return dooropen
+
+	else print "Alarm had went off, Game over! type 'python conditionals.py' to restart"
+
+
+def main():
+	incertcode = raw_input("Type the password: ")
+	correctpass = 
