971acd090e27780cf30521ab4bd5d4c53ac85310
cmis <cmis@cmis-hs04.(none)>
2016-04-22 13:36:44 +0700

    adder

---
+++ coutup.py
@@ -0,0 +1,25 @@
+#def countdown (n):
+	#if n<= 0:
+	#	print "blastoff!"
+
+	#else:
+		#print n
+		#countdown(n-1)
+
+
+#def countdown_from_to(start, stop):
+	
+	#if start >= stop:
+	#	print "done"
+	#else:
+	#	print start
+	#	countdown_from_to(start+1,stop)
+	#	
+#countdown_from_to(0,99)
+
+def adder():
+	if Next_num != "":
+		 raw_input("Next Number: ")
+adder()
+
+ 
