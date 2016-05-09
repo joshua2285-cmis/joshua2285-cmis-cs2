e1637b0ad1b2daa242f4a3ecce86070314154f3d
cmis <cmis@cmis-hs04.(none)>
2016-05-09 08:41:17 +0700

    almost done

--- oneguess2,0.py
+++ oneguess2,0.py
@@ -1,25 +1,37 @@
 #import
 import random
 
-thenumber = int(random.randint(0,100))
-print thenumber
+thenumber = str(random.randint(0,100))
+
 def guess(numberoftimes = 0):
 	guessnumber = str(raw_input("next: "))
-	if thenumber == guessnumber:
-		print "Good job"
 	
-	elif thenumber > guessnumber:
-		print "Too high"
-		numberoftimes + 1
-		guess(numberoftimes)
-
-	elif thenumber < guessnumber:
-		print "Too low"
-		numberoftimes + 1
-		guess(numberoftimes)
-
-	elif numberoftimes >= 5:
+	if numberoftimes == 5:
 		print "you fail"
 
-guess()
+	elif thenumber == guessnumber:
+		print "Good job"
 	
+	elif thenumber < guessnumber:
+		print "Too high"
+		numberoftimes += 1
+		guess(numberoftimes)
+
+	elif thenumber > guessnumber:
+		print "Too low"
+		numberoftimes += 1
+		guess(numberoftimes)
+
+	
+def main(Round = 0):
+	Round += 1
+	print Round
+
+	if Round == 3:
+		print "Thanks for playing."
+	else:
+		
+		guess()
+		main()
+	
+main()
