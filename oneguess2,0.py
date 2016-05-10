db8bf6a266a724de30d6bb38f7628c80871bd9a6
cmis <cmis@cmis-hs04.(none)>
2016-05-10 10:54:23 +0700

    Almost done, Haven't figerout where to put the random number

--- oneguess2,0.py
+++ oneguess2,0.py
@@ -1,10 +1,10 @@
 #import
 import random
 
-thenumber = str(random.randint(0,100))
+thenumber = int(random.randint(0,100))
 
 def guess(numberoftimes = 0):
-	guessnumber = str(raw_input("next: "))
+	guessnumber = int(raw_input("next: "))
 	
 	if numberoftimes == 5:
 		print "you fail"
@@ -22,16 +22,17 @@ def guess(numberoftimes = 0):
 		numberoftimes += 1
 		guess(numberoftimes)
 
-	
-def main(Round = 0):
-	Round += 1
-	print Round
 
-	if Round == 3:
+def main(Round = 1):
+	if Round == 4:
 		print "Thanks for playing."
 	else:
-		
+		print Round
+		Round += 1
+		thenumber = int(random.randint(0,100))
 		guess()
-		main()
+		main(Round)
+		
 	
 main()
+
