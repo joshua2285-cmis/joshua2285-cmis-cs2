b69604ed2d7bcfc7794eca2237b0a807380693a5
cmis <cmis@cmis-hs04.(none)>
2016-05-13 08:39:23 +0700

    expanding it to four function

--- conditionals.py
+++ conditionals.py
@@ -1,34 +1,29 @@
+# The player had been trap inside a locked room, there is a key pad on the side of the door 
 print "you are locked in a room and there are many different doors with passcode and you are trying to break free."
 
 #import
 import random
-import time
 
+# Correct pass
+def correctpass():
+	correctpass = random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9)
+	return correctpass
 
+# Compare
+def compare():
+	incertcode = str(raw_input("Type the password: "))
+	correctpass = correctpass()
 
-# Time
-#import time
-#run = raw_input("Ready? > ")
-#seconds = 1
-#if run == "Ready":
-#	while seconds != 6:
-#		print "You used ", seconds
-#		time.sleep(1)
-#		seconds +=1
+	if incertcode == correctpass:
+		return True
+
+	else:
+		return False
 
 # Main
 def main():
-	correctpass = random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9)
-	print correctpass
-	incertcode = raw_input("Type the password: ")
-
-	if incertcode == correctpass:
-		print "Good Job your are free now!!!! You won!"
-
-	else:
-		print "Alarm had went off, Game over!"
+	
+	
 
 	
 main()
-
-# http://stackoverflow.com/questions/18722753/trying-to-generate-a-series-of-unique-random-numbers
