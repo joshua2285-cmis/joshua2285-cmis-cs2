0e2d14b3b6cc7b8e48344866243c9e522ce0ab71
cmis <cmis@cmis-hs22.(none)>
2016-05-24 13:35:44 +0700

    Testing out with whileloops

---
+++ lol.py
@@ -0,0 +1,36 @@
+#while 1==1:
+#	print "01",
+
+
+
+#x = 1
+#while x != -1:
+#	print x
+#	x+= 856342333452354353245243523452452345224535652347237684567756356456567547
+
+#def countup():
+#	x = 1
+#	while x!= 100:
+#		print x
+#		x+=1
+
+#countup()
+
+#def countdown():
+#	x = 100
+#	while x!= 0:
+#		print x
+#		x-=1
+
+#countdown()
+
+def counting():
+	x = raw_input("The Number: ")
+	while x < 0:
+		print x
+		x +=n
+	while x >0:
+		print x
+		x-=n
+	while x = 0:
+		pass
