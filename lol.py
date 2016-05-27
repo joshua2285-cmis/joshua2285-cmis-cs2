b99de33b5454cc0609a6c9bf02d2c5c0f1a513f6
cmis <cmis@cmis-hs04.(none)>
2016-05-27 10:55:59 +0700

    working on grid

--- lol.py
+++ lol.py
@@ -6,7 +6,7 @@
 #x = 1
 #while x != -1:
 #	print x
-#	x+= 856342333452354353245243523452452345224535652347237684567756356456567547
+#	x*= 856342333452354353245243523452452345224535652347237684567756
 
 #def countup():
 #	x = 1
@@ -46,9 +46,28 @@
 #		x -=1
 #countfrom2(20,12)
 
+#def sumOfOdds(n):
+#	sum = 0
+#	if n>0:
+#		while n > 0:
+#			if n%2 ==1:
+#				sum+= n
+#			n -= 1
+#	elif n<0:
+#		while n<0:
+#			if n%2 ==1:
+#				sum += n
+#			n+=1
+#		return sum
+#print sumOfOdds(10)
 
-def add0dds(n):
-	if n<0:
+
+def grid(w,h):
+	while h !=0:
+		while w!= 0:
+			print ".",
+			w-=1
+		h-=1
 		
-	elif n>0:
+grid(10,10)
 
